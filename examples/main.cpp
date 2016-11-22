8d6342b9b58e81efb1825d43a5e5aa560ece14bf
Mark Oates <euph_boy@hotmail.com>
2016-11-21 21:57:49 -0500

    Convert tabs to 2 spaces in examples/main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -9,70 +9,70 @@
 class Project : public NewWorldScreen
 {
 public:
-	BitmapBin bitmaps;
-	Motion motion;
+   BitmapBin bitmaps;
+   Motion motion;
    TileIndex tile_index;
 
-	Project(Display *display, Player *player)
-		: NewWorldScreen(display, player)
-		, bitmaps("data")
-		, motion()
+   Project(Display *display, Player *player)
+      : NewWorldScreen(display, player)
+      , bitmaps("data")
+      , motion()
       , tile_index()
-	{
-		level = new Level();
-		current_map = new Map();
-		level->maps.push_back(current_map);
+   {
+      level = new Level();
+      current_map = new Map();
+      level->maps.push_back(current_map);
 
-		tile_index.load_from_atlas(bitmaps["spritesheet.png"], 21, 21, 2, 2, 1, 1);
-		current_map->tile_layers.push_back(TileLayer(&tile_index, 60, 60));
+      tile_index.load_from_atlas(bitmaps["spritesheet.png"], 21, 21, 2, 2, 1, 1);
+      current_map->tile_layers.push_back(TileLayer(&tile_index, 60, 60));
 
-		// fill our map with random tiles
-		for (unsigned x=0; x<current_map->tile_layers.back().width; x++)
-			for (unsigned y=0; y<current_map->tile_layers.back().height; y++)
-				current_map->tile_layers.back().set_tile(x, y, random_int(0, 200));
-	}
+      // fill our map with random tiles
+      for (unsigned x=0; x<current_map->tile_layers.back().width; x++)
+         for (unsigned y=0; y<current_map->tile_layers.back().height; y++)
+            current_map->tile_layers.back().set_tile(x, y, random_int(0, 200));
+   }
 
-	void update_reposition_camera()
-	{
-	   static float reposition_timer = 0;
-		// update the camera's scale and rotation
+   void update_reposition_camera()
+   {
+      static float reposition_timer = 0;
+      // update the camera's scale and rotation
 
-		// initiate motion with the camera
-		reposition_timer -= 1/60.0;
-		if (reposition_timer <= 0)
-		{
-			reposition_timer = 3;
+      // initiate motion with the camera
+      reposition_timer -= 1/60.0;
+      if (reposition_timer <= 0)
+      {
+         reposition_timer = 3;
          motion.cmove_to(&camera.placement.position.x, random_float(0, 40*21), 3, interpolator::double_slow_in_out);
          motion.cmove_to(&camera.placement.position.y, random_float(0, 20*21), 3, interpolator::double_slow_in_out);
-		}
+      }
 
-		camera.placement.scale.x = sin(al_get_time())*0.1 + 2;
-		camera.placement.scale.y = camera.placement.scale.x;
-		camera.placement.rotation = sin(al_get_time())*TAU * 0.03;
-	}
+      camera.placement.scale.x = sin(al_get_time())*0.1 + 2;
+      camera.placement.scale.y = camera.placement.scale.x;
+      camera.placement.rotation = sin(al_get_time())*TAU * 0.03;
+   }
 
-	void primary_timer_func() override
-	{
-		motion.update(Framework::time_now);
+   void primary_timer_func() override
+   {
+      motion.update(Framework::time_now);
 
       update_reposition_camera();
 
-		NewWorldScreen::primary_timer_func();
-	}
+      NewWorldScreen::primary_timer_func();
+   }
 };
 
 
 
 int main(int argc, char *argv[])
 {
-	Framework::initialize();
-	Display *display = Framework::create_display(800, 600);
+   Framework::initialize();
+   Display *display = Framework::create_display(800, 600);
 
-	NewWorldScreen *world_screen = new Project(display, NULL);
+   NewWorldScreen *world_screen = new Project(display, NULL);
 
-	Framework::run_loop();
+   Framework::run_loop();
 
-	return 0;
+   return 0;
 }
 
 
